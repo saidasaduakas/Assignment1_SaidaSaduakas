23d6020390892b39de6fb9551c8b45da627d7490
saidasaduakas <80287291+saidasaduakas@users.noreply.github.com>
2021-09-26 22:40:15 +0600

    Create test.py

---
+++ test.py
@@ -0,0 +1,14 @@
+from typing import Counter
+from typing import Counter
+from src1 import*
+
+var = input ("Enter number :")
+
+counter = 0 
+for i in coing:
+
+    if counter == int(var):
+        break
+
+    print (counter + 1, coing [counter]['id'],coing[counter]['symbol'], coing[counter]['market_cap'])
+    counter +=1
